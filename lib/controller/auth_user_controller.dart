f9a3e671661979ef9f1a24b66180c9b7fd15267d
Samuel Obour Mensah <msamuelobourmensah@gmail.com>
2024-02-22 22:20:45 +0000

    commit

--- lib/controller/auth_user_controller.dart
+++ lib/controller/auth_user_controller.dart
@@ -11,25 +11,36 @@ class AuthController {
 
     try {
       // we will first create user account in the auth tab after then cloud firestore
-     UserCredential  userCredential =  await _auth.createUserWithEmailAndPassword(
-          email: email, password: password);
+      UserCredential userCredential = await _auth
+          .createUserWithEmailAndPassword(email: email, password: password);
       res = "Success";
-await _firebaseFirestore.collection("buyers").doc(userCredential.user!.uid).set(
-{
-  "fullName":fullName,
-  'profileImage': '',
-  "email":email,
-  'pinCode': '',
-  'userId':userCredential.user!.uid,
-  'locallity':"",
-  "city":"",
-  "state":"",
-}
-
-
-);
-
+      await _firebaseFirestore
+          .collection("buyers")
+          .doc(userCredential.user!.uid)
+          .set({
+        "fullName": fullName,
+        'profileImage': '',
+        "email": email,
+        'pinCode': '',
+        'userId': userCredential.user!.uid,
+        'locallity': "",
+        "city": "",
+        "state": "",
+      });
     } catch (e) {}
+    return res;
+  }
+
+  // LOGIN USER
+  Future<String> loginUser(String email, String password) async {
+    String res = "Somethig went wrong";
+
+    try {
+      await _auth.signInWithEmailAndPassword(email: email, password: password);
+      res = "success";
+    } catch (e) {
+      res = e.toString();
+    }
     return res;
   }
 }
